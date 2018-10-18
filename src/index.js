06bc3ef6e079f4d177a5f7104a7d929f6b770854
unknown <development.4reg.2qt3@gmail.com>
2018-10-19 02:28:23 +0900

    パッケージの追加等の初期設定

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const https = require('https');
+const url = require('url');
+const path = require('path');
+const fs = require('fs');
+const { google } = require('googleapis');
+
+const scopes = [
+    'https://www.googleapis.com/auth/calendar',
+    'https://www.googleapis.com/auth/calendar.events',
+    'https://www.googleapis.com/auth/calendar.events.readonly',
+    'https://www.googleapis.com/auth/calendar.readonly',
+    'https://www.googleapis.com/auth/calendar.settings.readonly'
+];
+
+
+let keys = {
+    client_id: '',
+    client_secret: '',
+    redirect_uris: ''
+};
+
+if (fs.existsSync(__dirname, '../private/client_secret.json')) {
+    const keyPath = path.join(__dirname, '../private/client_secret.json');
+    keys = fs.readFileSync(keyPath).web;
+} else {
+    keys.client_id = 'test',
+    keys.client_secret = 'test',
+    keys.redirect_uris = 'test'
+}
+
+console.log(keys);
