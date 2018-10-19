0bf1f8f47da7c1fd4d3cdba434eabea7b61628e4
unknown <development.4reg.2qt3@gmail.com>
2018-10-20 05:59:19 +0900

    mod index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const http = require('http');
 const https = require('https');
 const url = require('url');
 const path = require('path');
+const querystring = require('querystring');
 const fs = require('fs');
 const { google } = require('googleapis');
 const opn = require('opn');
@@ -14,38 +15,18 @@ const scopes = [
     'https://www.googleapis.com/auth/calendar.settings.readonly'
 ];
 
-
-const keyPath = path.join(__dirname, '../private/client_secret.json');
-
-let keys = {
-    client_id: '',
-    client_secret: '',
-    redirect_uris: ''
-};
-
-if (fs.existsSync(keyPath)) {
-    keys = JSON.parse(fs.readFileSync(keyPath)).web;
-} else {
-    keys.client_id = process.env.CLIENT_ID,
-    keys.client_secret = process.env.CLIENT_SECRET,
-    keys.redirect_uris = process.env.REDIRECT_URIS.split(' ')
-}
-
 const oauth2Client = new google.auth.OAuth2(
-    keys.client_id,
-    keys.client_secret,
-    keys.redirect_uris[0]
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    process.env.REDIRECT_URIS.split(' ')
 );
-
-const authorizeUrl = oauth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: scopes
-});
-
-opn(authorizeUrl);
 
 const server = http.createServer();
 
 server.on('request', (req, res) => {
-    console.log(req.url)
-}).listen(process.env.PORT||8080);
+    switch (req.method) {
+        case 'GET':
+            console.log(querystring.parse(url.parse(req.url)))
+    }
+}).listen(process.env.PORT);
+
