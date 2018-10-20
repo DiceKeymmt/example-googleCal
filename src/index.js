7722ef6603efc1532f8a549d72fb9cd235aed829
unknown <development.4reg.2qt3@gmail.com>
2018-10-21 01:39:27 +0900

    mod index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
+require('dotenv').config();
 const http = require('http');
 const https = require('https');
 const url = require('url');
 const path = require('path');
 const querystring = require('querystring');
 const fs = require('fs');
-const { google } = require('googleapis');
+const {
+    google
+} = require('googleapis');
 const opn = require('opn');
 
 const scopes = [
@@ -18,21 +21,70 @@ const scopes = [
 const oauth2Client = new google.auth.OAuth2(
     process.env.CLIENT_ID,
     process.env.CLIENT_SECRET,
-    process.env.REDIRECT_URIS.split(' ')
+    process.env.REDIRECT_URIS.split(' ')[1]
 );
 
-const server = http.createServer();
+async function authenticate() {
+    return new Promise((resolve, reject) => {
+        const authUrl = oauth2Client.generateAuthUrl({
+            access_type: 'offline',
+            prompt: 'consent',
+            scope: scopes
+        });
+        if (process.env.REFRESH_TOKEN) {
+            oauth2Client.setCredentials({
+                refresh_token: process.env.REFRESH_TOKEN
+            });
 
-server.on('request', (req, res) => {
-    switch (req.method) {
-        case 'GET':
-            console.log('get')
-            console.log(querystring.parse(url.parse(req.url)))
-            break;
-        
-        default:
-            console.log('not get');
-            break;
-    }
-}).listen(process.env.PORT);
+            return resolve(oauth2Client);
+        }
 
+        const server = http.createServer(async (req, res) => {
+            opn(authUrl);
+            if (url.parse(req.url).pathname === '/oauth2callback') {
+                const qs = querystring.parse(url.parse(req.url).query);
+                server.close();
+                const {
+                    tokens
+                } = await oauth2Client.getToken(qs.code);
+                oauth2Client.setCredentials(tokens);
+                resolve(oauth2Client);
+            }
+        }).listen(process.env.PORT || 8080);
+    });
+}
+
+authenticate()
+    .then(client => {
+        const calendar = google.calendar({
+            version: 'v3',
+            auth: client
+        });
+
+        const server = http.createServer();
+
+        server.on('request', (req, res) => {
+            switch (url.parse(req.url).pathname) {
+                case '/calendars':
+                    switch (req.method) {
+                        case 'GET':
+                        const qs = querystring.parse(url.parse(req.url).query);
+                        return calendar.calendars.get({
+                            calendarId: qs.calendarid
+                        }, (err, r) => {
+                            res.writeHead(200, {
+                                'Content-Type': 'application/json'
+                            });
+                            res.write(JSON.stringify(r.data));
+                            res.end();
+                        })
+
+                        default:
+                            res.end('404\nFile Not Found');
+                    }
+                
+                default:
+                    res.end('404\nFile Not Found');
+            }
+        }).listen(process.env.PORT||8080);
+    });
